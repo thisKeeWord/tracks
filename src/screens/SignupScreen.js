f9ca3b7c8104f5bb4453164ba121ead158c836d8
Leonard <leonardwkee@gmail.com>
2021-12-30 23:29:41 -0800

    part 15.1 - 15.17: adding tracks now

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,54 @@
+import React, { useState, useContext } from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Text, Input, Button } from 'react-native-elements'
+import { Context as AuthContext } from '../context/AuthContext'
+import Spacer from '../components/Spacer'
+
+const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer />
+      <Input
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer>
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
+      </Spacer>
+    </View>
+  )
+}
+
+SignupScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
+  },
+})
+
+export default SignupScreen
