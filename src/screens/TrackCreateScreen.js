97aa525e239817e7112b846654436f3066b2112d
Leonard <leonardwkee@gmail.com>
2022-01-01 14:16:17 -0800

    part 15.43 - 15.49: add mockLocation

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView } from 'react-native'
 import { View, StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
+import { requestForegroundPermissionsAsync } from 'expo-location'
 import Map from '../components/Map'
 
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null)
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync()
+      if (!granted) {
+        throw new Error('Location permission not granted')
+      }
+    } catch (e) {
+      setErr(e)
+    }
+  }
+
+  useEffect(() => {
+    startWatching()
+  }, [])
+
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2>Create a Track</Text>
       <Map />
+      {err ? <Text>Please enable location services.</Text> : null}
     </SafeAreaView>
   )
 }
