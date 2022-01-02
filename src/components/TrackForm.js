1bcddaf92fcce576238a43edd37ffc2ee3ee48cd
Leonard <leonardwkee@gmail.com>
2022-01-01 17:20:49 -0800

    part 15.15 - 15.66: add location subscribing

---
+++ src/components/TrackForm.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import { Input, Button } from 'react-native-elements'
+import { Context as LocationContext } from '../context/LocationContext'
+import Spacer from './Spacer'
+
+const TrackForm = () => {
+  const { state, startRecording, stopRecording, changeName } = useContext(LocationContext)
+  const { name, recording, locations } = state
+
+  return (
+    <>
+      <Spacer>
+        <Input value={name} onChangeText={changeName} placeholder="Enter name" />
+      </Spacer>
+      <Spacer>
+        <Button title={recording ? 'Stop' : 'Start Recording'} onPress={recording ? stopRecording : startRecording} />
+      </Spacer>
+    </>
+  )
+}
+
+
+export default TrackForm
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,17 +1,20 @@
 import '../_mockLocation'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView, withNavigationFocus } from 'react-navigation'
 import { Context as LocationContext } from '../context/LocationContext'
 import useLocation from '../hooks/useLocation'
 import Map from '../components/Map'
+import TrackForm from '../components/TrackForm'
 
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { addLocation } = useContext(LocationContext)
-  const [err] = useLocation(isFocused, addLocation)
-
+  const { state, addLocation } = useContext(LocationContext)
+  const callback = useCallback((location) => {
+    addLocation(location, state.recording)
+  }, [state.recording])
+  const [err] = useLocation(isFocused, callback)
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
@@ -19,6 +22,7 @@ const TrackCreateScreen = ({ isFocused }) => {
       <Map />
       <NavigationEvents onWillBlur={() => console.log('leaving')} />
       {err ? <Text>Please enable location services.</Text> : null}
+      <TrackForm />
     </SafeAreaView>
   )
 }
